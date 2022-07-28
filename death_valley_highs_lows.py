beee04b7380b905f5f1650252901d18e09fc92d9
Wesley Stagg <wesleystagg@users-MacBook-Air.local>
2022-07-28 11:34:24 +0100

    Initial Commit

---
+++ death_valley_highs_lows.py
@@ -0,0 +1,42 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+filename = 'data/death_valley_2018_simple.csv'
+with open(filename) as f:
+	reader = csv.reader(f)
+	header_row = next(reader)
+	
+
+
+	# Get dates and high temprature from the file.
+	dates, highs, lows = [], [], []
+	for row in reader:
+		current_date = datetime.strptime(row[2], '%Y-%m-%d')
+		try:
+			high = int(row[4])
+			low = int(row[5])
+		except ValueError:
+			print(f"Missing data for {current_date}")
+		else:
+			dates.append(current_date)
+			highs.append(high)
+			lows.append(low)
+
+	# plot the high and low temperatures
+	plt.style.use('seaborn')
+	fig, ax = plt.subplots()
+	ax.plot(dates, highs, c='red',alpha=0.5)
+	ax.plot(dates, lows, c='blue', alpha=0.5)
+	ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+	
+
+	# format plot
+	title = 'Daily high and low temperatures - 2018\nDeath valley, CA'
+	ax.set_title(title, fontsize=20)
+	ax.set_xlabel('', fontsize=16)
+	fig.autofmt_xdate()
+	ax.set_ylabel('Temprature (F)', fontsize=16)
+	ax.tick_params(axis='both', which='major', labelsize=16)
+
+	plt.show()
---
+++ sitka_rainfall.py
@@ -0,0 +1,42 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+filename = 'data/sitka_weather_2018_simple.csv'
+with open(filename) as f:
+	reader = csv.reader(f)
+	header_row = next(reader)
+	
+
+
+	# Get dates and high temprature from the file.
+	dates, rain = [], []
+	for row in reader:
+		current_date = datetime.strptime(row[2], '%Y-%m-%d')
+		prcp = float(row[3])
+		dates.append(current_date)
+		rain.append(prcp)
+
+
+	# plot the high and low temperatures
+	plt.style.use('seaborn-pastel')
+	fig, ax = plt.subplots()
+	ax.plot(dates, rain, c='blue')
+	# ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+	
+
+	# format plot
+	ax.set_title('Daily rainfall Sitka - 2018', fontsize=24)
+	ax.set_xlabel('', fontsize=16)
+	fig.autofmt_xdate()
+	ax.set_ylabel('Rainfall (mm)', fontsize=16)
+	ax.tick_params(axis='both', which='major', labelsize=16)
+
+	plt.show()
+
+
+	
+
+
+
+
